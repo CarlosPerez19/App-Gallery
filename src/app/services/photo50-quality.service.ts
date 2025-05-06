dd645757808684bcab09b69d9e51b53c776d2e62
DNeekoD <carlos.perez02@epn.edu.ec>
2025-05-06 09:18:49 -0500

    Photo 50%

--- src/app/services/photo50-quality.service.ts
+++ src/app/services/photo50-quality.service.ts
@@ -80,7 +80,7 @@ export class Photo50QualityService {
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
-      quality: 100
+      quality: 50
     });
   
     const savedImageFile = await this.savePicture(capturedPhoto);
