be480490231d1be69a916c8513359233cc031f02
DNeekoD <carlos.perez02@epn.edu.ec>
2025-05-18 19:20:20 -0500

    SplashScreen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // milisegundos (3 segundos)
+      launchAutoHide: true,
+      backgroundColor: '#000000', // negro
+      anddroidSplarshScreen: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
